a4e76a98ea65072ed46abcda93e3906ddf722247
patel-purvak <hello2purvak@gmail.com>
2024-07-25 16:58:19 -0400

    Added username and password variable and button click event

--- app/src/main/java/com/purvak/littlelemon/LoginActivity.kt
+++ app/src/main/java/com/purvak/littlelemon/LoginActivity.kt
@@ -1,6 +1,7 @@
 package com.purvak.littlelemon
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -15,11 +16,17 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
 import com.purvak.littlelemon.ui.theme.LittleLemonTheme
 
@@ -42,6 +49,13 @@ class LoginActivity : ComponentActivity() {
 
 @Composable
 fun LoginScreen() {
+    val context = LocalContext.current
+    var username by remember {
+            mutableStateOf(TextFieldValue(""))
+        }
+    var password by remember {
+            mutableStateOf(TextFieldValue(""))
+        }
     Column(
         modifier = Modifier.fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally,
@@ -51,9 +65,27 @@ fun LoginScreen() {
             painter = painterResource(id = R.drawable.littlelemonlogo),
             contentDescription = "Logo Image"
         )
-        TextField(value = "", onValueChange = {}, label = { Text(text = "Username") })
-        TextField(value = "", onValueChange = {}, label = { Text(text = "Password") })
-        Button(onClick = { /*TODO*/ }
+        TextField(value = username, onValueChange = { username = it }, label = { Text(text = "Username") })
+        TextField(value = password, onValueChange = { password = it }, label = { Text(text = "Password") })
+        Button(onClick = {
+            // Handle login button click
+            if (username.text == "admin" && password.text == "password") {
+                // Login successful
+                // Navigate to the main activity
+                Toast.makeText(context,
+                    "Welcome to Little Lemon!",
+                    Toast.LENGTH_LONG
+                ).show()
+            } else {
+                // Login failed
+                // Show error message
+                Toast.makeText(context,
+                    "Invalid credentials."
+                            + "Please try again.",
+                    Toast.LENGTH_LONG
+                ).show()
+            }
+         }
             , colors = ButtonDefaults.buttonColors(
                 Color(0xFF495E57))
             ) {
